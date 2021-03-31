90238607bc6fc5775f3471154e8aa967187b1152
Galla299802 <galla299802@tamu.edu>
2021-03-30 22:43:39 -0500

    Added email field to users table and created the displayed_questions table

--- db/migrate/20210329024257_create_comments.rb
+++ db/migrate/20210329024257_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer "commenter_id"
-      t.string  "comment"
+      t.integer   "commenter_id"
+      t.string    "comment"
       t.datetime  "comment_date"
-      t.integer      "recipient_ID"
+      t.integer   "recipient_ID"
       t.timestamps
     end
   end
