ac33649888373bdc87fe311274168517b01dcaa0
clayjuneau <clayjuneau@gmail.com>
2021-04-07 12:46:45 -0500

    Create group and data controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'data/index'
+  get 'group/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Defines the start page of the application
