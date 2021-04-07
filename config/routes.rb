3b825f104a2855b2671d9819cc9adbc0ed0265e5
Dawson Abner <dawsonabner@gmail.com>
2021-04-06 21:45:47 -0500

    Homework index and controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'homework/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Defines the start page of the application
