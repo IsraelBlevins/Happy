15143fc58e03b3878fc5638b86758600741715b7
Galla299802 <galla299802@tamu.edu>
2021-04-09 12:45:23 -0500

    Added conditionals so that only admins can see the view all users links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.order(params[:group_id])
   end
 
   # GET /users/1 or /users/1.json
