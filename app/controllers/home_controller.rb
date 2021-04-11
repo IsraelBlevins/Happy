4d1df81930ed3746a50d1de53ac0a2d806cfe97a
Dawson Abner <dawsonabner@gmail.com>
2021-04-10 20:57:29 -0500

    Check in layout fixes and progress on check in functions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,25 @@ class HomeController < ApplicationController
   def index
     @displayed_questions = DisplayedQuestion.all()
     @comments = Comment.all()
+    @user_responses = UserResponse.all()
   end
+
+  def new
+    @user_response = UserResponse.new
+  end
+
+  def create_response
+    @user_response = UserResponse.new(user_response_params)
+
+    respond_to do |format|
+      if @user_response.save
+        format.html {redirect_to @user_response, notice: 'Response has been saved.'}
+        format.json {render :show, status: :created, location: @user_response}
+      else
+        format.html {render :new}
+        format.json {render json: @user_response.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
 end
