8806f8b0b6cd88822a27a6aee942d5c5887d5ea9
Dawson Abner <dawsonabner@gmail.com>
2021-04-11 23:45:40 -0500

    removed unused route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'sessions/new' => 'sessions#new'
   get 'home/index' => 'home#index'
   post '/create' => 'home#create'
-  post '/respond' => 'home#respond'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
 
