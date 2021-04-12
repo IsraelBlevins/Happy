caa0695bcfaf3d42caf1f30c11807e585bdcf28a
Christopher Gallardo <galla299802@tamu.edu>
2021-04-12 18:42:42 -0500

    Added morning boolean

--- db/migrate/20210412223334_create_mood_ratings.rb
+++ db/migrate/20210412223334_create_mood_ratings.rb
@@ -3,6 +3,7 @@ class CreateMoodRatings < ActiveRecord::Migration[6.0]
     create_table :mood_ratings do |t|
       t.integer "user_id"
       t.integer "rating"
+      t.boolean "morning"
       t.timestamps
     end
   end
