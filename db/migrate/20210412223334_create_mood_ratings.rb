070e7ab73ae2ead1995826f32ccc5937cd932971
Christopher Gallardo <galla299802@tamu.edu>
2021-04-12 18:15:32 -0500

    Added ratings table

---
+++ db/migrate/20210412223334_create_mood_ratings.rb
@@ -0,0 +1,9 @@
+class CreateMoodRatings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :mood_ratings do |t|
+      t.integer "user_id"
+      t.integer "rating"
+      t.timestamps
+    end
+  end
+end
